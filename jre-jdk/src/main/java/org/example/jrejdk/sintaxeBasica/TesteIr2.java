6b230d4b468111ce8efa49d30dda95ce00d632d6
joelma <joelmadev@gmail.com>
2022-11-24 20:21:48 -0300

    condicionais e boolean

---
+++ jre-jdk/src/main/java/org/example/jrejdk/sintaxeBasica/TesteIr2.java
@@ -0,0 +1,15 @@
+package org.example.jrejdk.sintaxeBasica;
+
+/*
+De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142
+De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350
+De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636
+ */
+
+public class TesteIr2 {
+     double salariojoao = 3300.0;
+
+    if (salariojoao >= 1900.0 && salariojoao <= 2800.0){
+
+    }
+}
