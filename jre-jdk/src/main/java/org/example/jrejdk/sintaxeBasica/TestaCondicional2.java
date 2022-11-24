5718934a4c4c7e83f5b07da2f601fc569668c29d
joelma <joelmadev@gmail.com>
2022-11-24 13:30:39 -0300

    refator

--- jre-jdk/src/main/java/org/example/jrejdk/sintaxeBasica/TestaCondicional2.java
+++ jre-jdk/src/main/java/org/example/jrejdk/sintaxeBasica/TestaCondicional2.java
@@ -1,11 +1,21 @@
 package org.example.jrejdk.sintaxeBasica;
 
+
+//AND é representado pelo && e OR pelo ||.
 public class TestaCondicional2 {
     public static void main(String[] args) {
         System.out.println("Testando condicionais");
         int idade = 25;
         int quantidadePessoas = 0;
-    if (idade >= 18) {
+        boolean acompanhado = false;
+
+        int idade2 = 68;
+        boolean ehIdoso = idade2 >= 65;
+
+        System.out.println(ehIdoso);
+
+
+    if (idade >= 18 || acompanhado == true) { //true ou false, opcional
         System.out.println("Bem vindo,pode entrar");
     }
         else if(idade <= 18 && quantidadePessoas >= 2) {
